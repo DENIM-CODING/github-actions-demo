2037f6a50cdc3568e7fe2515439e08250ff691ce
DENIM <khannadenim@gmail.com>
2026-08-19 16:27:25 +0530

    Initial Java test project

---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    void shouldAddTwoNumbers() {
+        assertEquals(10, calculator.add(4, 6));
+    }
+
+    @Test
+    void shouldSubtractTwoNumbers() {
+        assertEquals(4, calculator.subtract(10, 6));
+    }
+
+    @Test
+    void shouldMultiplyTwoNumbers() {
+        assertEquals(24, calculator.multiply(4, 6));
+    }
+}
